f8879874f7b7bbfbbd5eb799f42c97601f077394
spancavil <soancavil@gmail.com>
2022-04-26 18:32:58 -0300

    Adding configurations for heroku

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn blog_project.wsgi
+web: python manage.py runserver 0.0.0.0:$PORT
--- blog_project/settings.py
+++ blog_project/settings.py
@@ -13,7 +13,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 import os
 from pathlib import Path
 
-import dj_database_url
+# Configure Django App for Heroku.
+import django_on_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -138,3 +139,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_URL = '/login'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# Configure Django App for Heroku.
+django_on_heroku.settings(locals())
